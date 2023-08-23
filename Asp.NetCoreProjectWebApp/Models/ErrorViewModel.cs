aead49df9c123ad539be43e3be8c6cd1f95da63b
farukyucel5 <farukyucel5@gmail.com>
2023-08-23 21:18:16 +0300

    First commit

---
+++ Asp.NetCoreProjectWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.NetCoreProjectWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
