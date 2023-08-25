7b1965156758f08e55254f5f58ff37af699798d2
farukyucel5 <farukyucel5@gmail.com>
2023-08-25 23:22:05 +0300

    Test module

---
+++ Asp.NetCoreProjectWebApp/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Asp.NetCoreProjectWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreProjectWebApp.Controllers;
+
+public class TestController:Controller
+{
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
