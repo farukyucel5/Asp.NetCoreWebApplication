208d7036901bdd1aa3e0a4c40a5af400c714ebb3
farukyucel5 <farukyucel5@gmail.com>
2023-08-26 23:09:04 +0300

    A table has been created in Db using code first approach

---
+++ Asp.NetCoreProjectWebApp/Migrations/20230826200321_KitapTurleriTablosuEkle.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Asp.NetCoreProjectWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class KitapTurleriTablosuEkle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "KitapTurleri",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Ad = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitapTurleri", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KitapTurleri");
+        }
+    }
+}
--- Asp.NetCoreProjectWebApp/Utility/UygulamaDbContext.cs
+++ Asp.NetCoreProjectWebApp/Utility/UygulamaDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Asp.NetCoreProjectWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Asp.NetCoreProjectWebApp.Utility;
 
 public class UygulamaDbContext:DbContext
 {
     public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options){}
+    public DbSet<KitapTuru> KitapTurleri { get; set; }
     
 
 }
