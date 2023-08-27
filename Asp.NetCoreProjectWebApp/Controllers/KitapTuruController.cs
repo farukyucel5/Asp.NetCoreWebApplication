8e55af9a3934b1689aabce11ff86ac06caf7f29d
farukyucel5 <farukyucel5@gmail.com>
2023-08-27 13:01:36 +0300

    A controller and view have been inserted

---
+++ Asp.NetCoreProjectWebApp/Controllers/KitapTuruController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreProjectWebApp.Controllers;
+
+public class KitapTuruController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
