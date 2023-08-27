9f8d88f1aac2ec86544f0830a12e99d9899890e0
farukyucel5 <farukyucel5@gmail.com>
2023-08-27 20:53:16 +0300

    Dependency injection

--- Asp.NetCoreProjectWebApp/Controllers/KitapTuruController.cs
+++ Asp.NetCoreProjectWebApp/Controllers/KitapTuruController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCoreProjectWebApp.Models;
+using Asp.NetCoreProjectWebApp.Utility;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCoreProjectWebApp.Controllers;
 
 public class KitapTuruController : Controller
 {
+    private readonly UygulamaDbContext _uygulamaDbContext;
+
+    public KitapTuruController(UygulamaDbContext context)
+    {
+        _uygulamaDbContext = context;
+    }
+    
     // GET
     public IActionResult Index()
     {
+        List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList();
         return View();
     }
 }
