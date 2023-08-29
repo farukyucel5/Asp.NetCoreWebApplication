ce24c298238bd1a9480d844aac876f7df86c678f
farukyucel5 <farukyucel5@gmail.com>
2023-08-29 22:04:35 +0300

    Adding a new action

--- Asp.NetCoreProjectWebApp/Models/KitapTuru.cs
+++ Asp.NetCoreProjectWebApp/Models/KitapTuru.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Asp.NetCoreProjectWebApp.Models;
 
@@ -6,7 +7,9 @@ public class KitapTuru
 {
     [Key]  //primary key
     public int Id { get; set; }
+    
     [Required] //not null
+    [DisplayName("Kitap Türü Adı")]
     public string Ad { get; set; }
     
 }
