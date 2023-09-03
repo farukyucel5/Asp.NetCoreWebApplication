d68259d2538b3913e33e8f4885aed2576c8c56d5
farukyucel5 <farukyucel5@gmail.com>
2023-09-03 18:30:03 +0300

    introduction to repository pattern

---
+++ Asp.NetCoreProjectWebApp/Models/KitapTuruRepositoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Asp.NetCoreProjectWebApp.Utility;
+
+namespace Asp.NetCoreProjectWebApp.Models;
+
+public class KitapTuruRepositoryRepository : Repository<KitapTuru>,IKitapTuruRepository
+{
+    private UygulamaDbContext _uygulamaDbContext;
+    public KitapTuruRepositoryRepository(UygulamaDbContext uygulamaDbContext) : base(uygulamaDbContext)
+    {
+        _uygulamaDbContext = uygulamaDbContext;
+    }
+
+    public void Update(KitapTuru kitapTuru)
+    {
+        _uygulamaDbContext.Update(kitapTuru);
+    }
+
+    public void Save()
+    {
+        _uygulamaDbContext.SaveChanges();
+    }
+}
---
+++ Asp.NetCoreProjectWebApp/Models/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+using Asp.NetCoreProjectWebApp.Utility;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCoreProjectWebApp.Models;
+
+public class Repository<T> : IRepository<T> where T :  class
+{
+    private readonly UygulamaDbContext _uygulamaDbContext;
+    internal DbSet<T> dbSet;
+
+    public Repository(UygulamaDbContext uygulamaDbContext)
+    {
+        _uygulamaDbContext = uygulamaDbContext;
+         dbSet = _uygulamaDbContext.Set<T>();
+    }
+    public IEnumerable<T> GetAll()
+    {
+        IQueryable<T> query = dbSet;
+        return query.ToList();
+    }
+
+    public T Get(Expression<Func<T, bool>> filtre)
+    {
+        IQueryable<T> query = dbSet;
+        query = query.Where(filtre);
+        return query.FirstOrDefault();
+    }
+
+    public void Add(T entity)
+    {
+        dbSet.Add(entity);
+    }
+
+    public void Remove(T entity)
+    {
+        dbSet.Remove(entity);
+    }
+
+    public void DeleteInterval(IEnumerable<T> entity)
+    {
+        dbSet.RemoveRange(entity);
+    }
+}
