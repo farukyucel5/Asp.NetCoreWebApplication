8df72b7fb5439e53481839491d94216160946564
farukyucel5 <farukyucel5@gmail.com>
2023-09-03 17:57:36 +0300

    introduction to repository pattern

---
+++ Asp.NetCoreProjectWebApp/Models/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Asp.NetCoreProjectWebApp.Models;
+
+public interface IRepository<T> where T: class
+{
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T, bool>> filtre);
+    void Add(T entity);
+    void Remove(T entity);
+    void DeleteInterval(IEnumerable<T> entity);
+
+}
