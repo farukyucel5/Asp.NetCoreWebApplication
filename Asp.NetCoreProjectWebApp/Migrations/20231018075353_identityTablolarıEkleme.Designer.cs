e3630844983e94b5b59411d038f388c5c5dcb79a
farukyucel5 <farukyucel5@gmail.com>
2023-10-18 10:57:35 +0300

    identity migration has been done

--- Asp.NetCoreProjectWebApp/Migrations/20231018075353_identityTablolarıEkleme.Designer.cs
+++ Asp.NetCoreProjectWebApp/Migrations/20231018075353_identityTablolarıEkleme.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Asp.NetCoreProjectWebApp.Utility;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Asp.NetCoreProjectWebApp.Migrations
 {
     [DbContext(typeof(UygulamaDbContext))]
-    partial class UygulamaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231018075353_identityTablolarıEkleme")]
+    partial class identityTablolarıEkleme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
